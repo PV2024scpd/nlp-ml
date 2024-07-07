2a3beda9f71e4503e645086d17e5fe0fc7a1ebd6
PV2024scpd <eswari.priya2022@gmail.com>
2024-07-07 14:11:29 +1000

    test

--- Assignment5/XCS224N-A5-master/src/submission/model.py
+++ Assignment5/XCS224N-A5-master/src/submission/model.py
@@ -184,6 +184,7 @@ class GPT(nn.Module):
 
         # forward the GPT model
         token_embeddings = self.tok_emb(idx) # each index maps to a (learnable) vector
+        print("exit token embeddings")
         position_embeddings = self.pos_emb[:, :t, :] # each position maps to a (learnable) vector
         x_input = self.drop(token_embeddings + position_embeddings)
 
